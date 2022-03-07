f8d7b4b8615b8d9237fbd3a1c3d55e98875efabc
Adrian Cañete <canete.adrian@gmail.com>
2022-03-07 14:23:25 -0300

    Atomic Design Challenge 2.0

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import "../styles/Menu.scss";
+import React from 'react';
+import '../styles/Menu.scss';
 
 const Menu = () => {
 	return (
 		<div className="Menu">
 			<ul>
 				<li>
-					<a href="/" className="title">
-						My orders
-					</a>
+					<a href="/" className="title">My orders</a>
 				</li>
 				<li>
 					<a href="/">My account</a>
@@ -19,6 +17,6 @@ const Menu = () => {
 			</ul>
 		</div>
 	);
-};
+}
 
 export default Menu;
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import "../styles/ProductItem.scss";
+import React from 'react';
+import '../styles/ProductItem.scss';
 
 const ProductItem = () => {
 	return (
 		<div className="ProductItem">
-			<img
-				src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-				alt=""
-			/>
+			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
 				<div>
 					<p>$120,00</p>
@@ -19,6 +16,6 @@ const ProductItem = () => {
 			</div>
 		</div>
 	);
-};
+}
 
 export default ProductItem;
