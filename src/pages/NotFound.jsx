5be9e69ce912a8ca19086caa9125ab3a3d520498
Adrian Cañete <canete.adrian@gmail.com>
2022-03-07 13:57:35 -0300

    Atomic Design Challenge 1.2

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,9 @@ const NotFound = () => {
 			<div className="not-found-info">
 				<h1 className="not-found-title">404 Not Found</h1>
 				<h2>This page is not found</h2>
-				<button className="primary-button">Go Home</button>
+				<form action="/" className="form">
+					<button className="primary-button">Go Home</button>
+				</form>
 			</div>
 		</div>
 	);
