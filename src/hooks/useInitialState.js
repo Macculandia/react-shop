f7fa74c75a8cdbdad2e366dfcad4c06358554ee5
Adrian Cañete <canete.adrian@gmail.com>
2022-03-11 15:47:35 -0300

    Atomic Design Challenge 4.0

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -13,9 +13,16 @@ const useInitialState = () => {
 			cart: [...state.cart, payload],
 		});
 	};
+	const removeFromCart = (indexValue) => {
+		setState({
+			...state,
+			cart: state.cart.filter((product, index) => index !== indexValue),
+		});
+	};
 	return {
 		state,
 		addToCart,
+		removeFromCart,
 	};
 };
 
